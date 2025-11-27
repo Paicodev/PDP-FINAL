e9ba5f81fcf325393001f27d00d87f9cc7b5e10c
Pablo Aguilar <paedmaag@gmail.com>
2025-11-27 03:14:30 -0300

    Se realizó una prueba para verificar si persisten las tareas.
    En esta prueba piloto creamos una variable que tenga todo lo que contiene la clase PersistenciaJSON. Luego una variable gestor a la que le "inyectamos" la forma de persistencia con archivos JSON. Luego mostramos la cantidad de tareas iniciales, y agregamos una tarea de prueba.
    Verificamos si se muestra la dificultad visual (no deberia fallar si hicimos bien la rehidratación de objeto).
    Por último simulamos un reinicio para verificar que persisten los archivos al crear un nuevo gestor.
    
    Co-Authored-By: Lara <207945603+laris2003@users.noreply.github.com>

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { gestorTareas } from './gestorTareas';
+import { PersistenciaJSON } from './PersistenciaJSON';
+
+console.log("--- INICIO DE PRUEBA ---");
+
+//guardamos la estrategia en una instancia de la clase PersistenciaJSON
+const estrategiaJSON = new PersistenciaJSON();
+
+// Iniciamos el gestor con la estrategia de persistencia deseada
+const gestor = new gestorTareas(estrategiaJSON);
+
+console.log(`Tareas iniciales: ${gestor.obtenerTodasLasTareas().length}`);
+
+// Agregamos una tarea
+console.log("Agregando tarea de prueba...");
+const tarea = gestor.agregarTarea("Prueba JSON", "Verificando si guarda", "Medio");
+console.log(`Tarea creada con ID: ${tarea.getId()}`);
+
+// Verificamos si tiene métodos (Si esto falla, la clase Tarea está mal)
+console.log(`Visual: ${tarea.getDificultadVisual()}`);
+
+// Simulamos un reinicio (si no tuviesemos persistencia, perderíamos todo)
+console.log("\n--- SIMULANDO REINICIO DE APP ---");
+//creamos variable nueva de gestorTareas, para simular reinicio
+const gestorNuevo = new gestorTareas(estrategiaJSON);
+const tareasRecuperadas = gestorNuevo.obtenerTodasLasTareas();
+
+console.log(`Tareas recuperadas del disco: ${tareasRecuperadas.length}`);
