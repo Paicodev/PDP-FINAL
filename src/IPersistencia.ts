e8e6c49ded2bdd7af75d2304319a2cfeca2fe2da
Lara <massaurasabigail@gmail.com>
2025-11-27 01:08:39 -0300

    Se creo la Interfaz IPersistencia para la abstracción.
    Se creó la abstracción IPersistencia para establecer un contrato estricto
    que deben cumplir los futuros módulos de almacenamiento.
    
    Justificación del Diseño (POO):
    Abstracción: Se define el "qué" (guardar/cargar) separado del "cómo".,
    Desacoplamiento: Esto permitirá que el Gestor de Tareas no dependa de implementaciones concretas (como archivos o bases de datos), facilitando la escalabilidad a futuro.
    
    Co-Authored-By: GeroA6 <198119886+GeroA6@users.noreply.github.com>

---
+++ src/IPersistencia.ts
@@ -0,0 +1,7 @@
+import { Tarea } from './Tarea'; 
+
+// Definimos el contrato: Cualquier cosa que quiera guardar tareas, debe tener estos dos métodos.
+export interface IPersistencia {
+    guardar(tareas: Tarea[]): void;
+    cargar(): Tarea[];
+}
