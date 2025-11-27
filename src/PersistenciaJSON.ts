c3d9c54fde14a2713aa1cfb4f39431e84502aa48
Pablo Aguilar <paedmaag@gmail.com>
2025-11-27 02:17:25 -0300

    Se implementó la estrategia de persistencia en JSON, con escritura, lectura, y transformación de objetos a JSON y viceversa
    Se creó la clase PersistenciaJSON implementando la interfaz IPersistencia. (Abstracción en POO)
    
    - Se utilizó el módulo nativo 'fs' para operaciones de I/O (Escritura y Lectura).
    
    - Se guarda el array de tareas haciendo uso del "contrato" de IPersistencia, invocando dentro de este metodo las operaciones para convertir arrays en JSON (stringify), y la operacion  para escribir los datos en el archivo JSON (writeFileSync).
    
    -En el metodo cargar hacemos un control para verificar que el archivo no esté vacío. Luego, dentro de un try, intentamos cargar el archivo (leerlo) con readFileSync, luego con JSON.parse, convertimos el JSON en un array de objetos. Por último mapeamos en una Tarea Nueva, haciendo uso de Programacion Funcional Pura.
    
    - Se implementó lógica de "rehidratación" en el método cargar() para transformar los datos planos del JSON en instancias válidas de la clase Tarea, recuperando así sus métodos y comportamientos.
    
    Co-Authored-By: Tomás Ariel Villegas <130164651+tomasville@users.noreply.github.com>

---
+++ src/PersistenciaJSON.ts
@@ -0,0 +1,70 @@
+import * as fs from 'fs';
+import { Tarea } from './Tarea';
+import { IPersistencia } from './IPersistencia';
+
+/* constante para la ruta del archivo JSON
+Para que la ruta del archivo sea inmutable y fácil de gestionar*/
+
+const RUTA_ARCHIVO = './tareas.json'; 
+
+//Se implementa la interfaz
+export class PersistenciaJSON implements IPersistencia {
+    
+    /**
+     * Guarda el array de tareas en un archivo de texto plano (JSON).
+     * (Programación Estructurada: Uso de I/O secuencial)
+     */
+    public guardar(tareas: Tarea[]): void {
+        // Manejo de errores
+        try {
+            // stringify convierte el array de tareas a formato JSON
+            const datos = JSON.stringify(tareas, null, 2);
+            // writeFileSync escribe los datos en el archivo especificado
+            fs.writeFileSync(RUTA_ARCHIVO, datos, 'utf-8');
+        } catch (error) {
+            console.error("Error al escribir en disco:", error);
+        }
+    }
+
+    /**
+     * Carga y reconstruye las tareas desde el archivo.
+     * (POO: Rehidratación de objetos)
+     */
+    public cargar(): Tarea[] {
+        // Robustez: Si el archivo no existe no rompemos nada.
+        if (!fs.existsSync(RUTA_ARCHIVO)) {
+            return [];
+        }
+
+        try {
+            //readFileSync lee el contenido del archivo
+            const datos = fs.readFileSync(RUTA_ARCHIVO, 'utf-8');
+            //parse convierte el JSON a un array de objetos
+            const json = JSON.parse(datos);
+
+            // MAPEO: Transformamos datos crudos (JSON) en Instancias Reales (Objetos Tarea)
+            // hacemos uso de una funcion flecha porque es mas conciso
+            return json.map((t: any) => {
+                 // 1. Instanciamos para recuperar los métodos de la clase
+                 const tarea = new Tarea(
+                    t.titulo, 
+                    t.descripcion, 
+                    t.dificultad, 
+                    t.fechaVencimiento ? new Date(t.fechaVencimiento) : undefined
+                 );
+                 
+                 // 2. Restauramos el estado interno (IDs, Fechas y Estado)
+                 // Usamos 'as any' para acceder a props privadas durante la carga
+                 (tarea as any).id = t.id;
+                 (tarea as any).estado = t.estado;
+                 (tarea as any).fechaCreacion = new Date(t.fechaCreacion);
+                 (tarea as any).ultimaEdicion = new Date(t.ultimaEdicion);
+                 
+                 return tarea;
+            });
+        } catch (error) {
+            console.error("Error al leer el archivo JSON:", error);
+            return [];
+        }
+    }
+}
